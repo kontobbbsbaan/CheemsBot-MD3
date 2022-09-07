0d7c06892c98e635b21bf10de108a6d876316101
Kian <108917081+kontobbbsbaan@users.noreply.github.com>
2022-09-07 10:26:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,26 +41,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['6283845409710'] //ur owner number
+global.ownername = "Kian"
+global.ytname = "" //ur yt chanel name
+global.socialm = "" //ur github or insta name
+global.location = "" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
+global.botname = "𝗠𝗢𝗗𝗭 𝗕𝗢𝗧𝗭" //ur bot name
 global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
 global.author = "🦄Dream Guy Xeon"
-global.themeemoji = "🐶"
-global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
-global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
-global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
-global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
+global.themeemoji = "🤓"
+global.reactmoji = "💬"
+global.ownertag = ['6283845409710'] //ur owner tag
+global.ownernummenu = ['6283845409710'] //ur owner number in menu and all
+global.watermark = "𝗠𝗢𝗗𝗭 𝗕𝗢𝗧𝗭" //ur watermark
+global.botscript = "Buy"
+global.linkz1 = "https://chat.whatsapp.com/FvDujLzzrQsGaJaRLVZ0Fg"
+global.linkz2 = "https://chat.whatsapp.com/FvDujLzzrQsGaJaRLVZ0Fg"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
